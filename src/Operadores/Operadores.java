40c840c5a6e9e436c11a98090650603435f8f2ba
Danilonesanto <danilo.espirito@gmail.com>
2023-10-05 23:14:33 -0300

    novas classes

---
+++ src/Operadores/Operadores.java
@@ -0,0 +1,8 @@
+package Operadores;
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+    }
+}
---
+++ src/Operadores/Ternario.java
@@ -0,0 +1,19 @@
+package Operadores;
+public class Ternario {
+    public static void main(String[] args) {
+        String nomeUm = "Danilo";
+        String nomeDois = "Danilo";
+
+        System.out.println(nomeUm.equals(nomeDois)); 
+        
+        
+        int a, b;
+        a = 5;
+        b = 5;
+
+        String resultado = a == b ? "Verdadeiro" : "Falso";
+
+            System.out.println(resultado);
+
+    }
+}
